cc7dbad53d2d11aea7e36608d4457718fff8d4a6
abeganskaya <forkeytms@gmail.com>
2023-04-25 19:17:24 +0300

    Home work 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+/* Example of multiline comment
+.........／＞　 フ.....................
+　　　　　| 　_　 _|
+　 　　　／`ミ _x 彡
+　　 　 /　　　 　 |
+　　　 /　 ヽ　　 ﾉ
+　／￣|　　 |　|　|
+　| (￣ヽ＿_ヽ_)_)
+　＼二つ
+ */
+
 public class Main
 {
+    //One line comment
     public static void main(String[] args)
     {
         System.out.println("My homeWork. Lesson 1");
