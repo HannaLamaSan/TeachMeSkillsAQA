f5f9ea1a403119e903d6de2990fe2cbacbfa14b8
abeganskaya <forkeytms@gmail.com>
2023-04-30 13:10:55 +0300

    Home work 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,21 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("My homeWork. Lesson 1");
+        System.out.println("Grid");
+        System.out.println("    |    |    ");
+        System.out.println("____|____|____");
+        System.out.println("    |    |    ");
+        System.out.println("____|____|____");
+        System.out.println("    |    |    ");
+        System.out.println("    |    |    ");
+        System.out.println("Tree");
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+
+
+        //как я понимаю елку можно сделать через циклы?
     }
 }
